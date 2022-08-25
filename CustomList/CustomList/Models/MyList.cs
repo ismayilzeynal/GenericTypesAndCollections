3729b566c3b8933b469dd2b43f7a917bea69b561
ismayilzeynal <ismayilbz@code.edu.az>
2022-08-25 13:49:07 +0400

    initial commit

---
+++ CustomList/CustomList/Models/MyList.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomList.Models
+{
+    class MyList<T>
+    {
+        ~MyList()
+        {
+            Console.WriteLine("Cleared");
+        }
+
+
+
+        T[] _arr;
+        int _counter;
+        public int Capacity { get; } = 10;
+        public int Count { get => _arr.Length - _counter; }
+        public MyList()
+        {
+            _arr = new T[0];
+        }
+
+
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < _arr.Length)
+                {
+                    return _arr[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                if (index < _arr.Length)
+                {
+                    _arr[index] = value;
+                }
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+
+
+        public void Add(T item)
+        {
+            if(_counter==0)
+            {
+                Array.Resize(ref _arr, _arr.Length + Capacity);
+                _counter = Capacity;
+            }
+
+            _arr[Count] = item;
+            _counter--;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(_arr[i], item))
+                    return i;
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public void Clear()
+        {
+            _counter = 0;
+            _arr = new T[0];
+        }
+
+        public void Remove(T item)
+        {
+            for (int i = IndexOf(item); i < _arr.Length-1; i++)
+            {
+                _arr[i]=_arr[i+1];
+            }
+            Array.Resize(ref _arr, _arr.Length - 1);
+        }
+
+        public bool Exist(T item)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (object.Equals(item, _arr[i]))
+                    return true;
+            }
+            
+            return false;
+        }
+
+        public void Reverse()
+        {
+            int len = _arr.Length;
+            for (int i = 0; i < len; i++)
+            {
+                T temp = _arr[i];
+                _arr[i] = _arr[len - i-1];
+                _arr[len - i - 1] = temp;
+            }
+        }
+
+        public int LastIndexOf(T item)
+        {
+            for (int i = _arr.Length-1; i >=0 ; i--)
+            {
+                if (object.Equals(_arr[i], item))
+                    return i;
+            }
+            throw new KeyNotFoundException();
+        }
+
+    }
+}
---
+++ CustomList/CustomList/Program.cs
@@ -0,0 +1,27 @@
+﻿using CustomList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> ml = new MyList<int>();
+            ml.Add(5);
+            ml.Add(3);
+            ml.Add(7);
+            ml.Add(48);
+            ml.Remove(48);
+            ml.Reverse();
+            ml.IndexOf(5);
+            ml.Add(3);
+            ml.Exist(79);
+            ml.LastIndexOf(3);
+            ml.Clear();
+
+        }
+    }
+    
+}
